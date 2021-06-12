3896769cb4e17c874fa1e87bfe51ea62ad2f3531
Ersocaut <leonardo.soy.yo@hotmail.com>
2021-06-12 00:24:51 -0500

    Tuples and map implementations

--- Contest Volumes (10000...)/Volume 114 (11400-11499)/11417.cpp
+++ Contest Volumes (10000...)/Volume 114 (11400-11499)/11417.cpp
@@ -1,6 +1,17 @@
 #include<iostream>
+#include<tuple>
+#include<map>
 
 using namespace std;
+map<tuple<int,int>,int> rev;
+
+bool eval(int a, int b){
+    tuple <int, int> ev(a,b);
+    if (rev.find(ev) != rev.end()){
+        return true;
+    }
+    return false;
+}
 
 int gcd(int a, int b){
     int g = 1;
@@ -9,6 +20,7 @@ int gcd(int a, int b){
             g = i;
         }
     }
+    rev[make_tuple(a,b)] = g;
     return g;
 }
 
@@ -16,7 +28,12 @@ int gcd(int a){
     int g = 0;
     for (int i = 1; i < a; i++){
         for (int j = i+1; j <= a; j++){
-            g = g + gcd(i,j);
+            if (eval(i,j)){
+                g += rev[make_tuple(i,j)];
+            }
+            else{
+                g = g + gcd(i,j);
+            }
         }
     }
     return g;
@@ -26,7 +43,7 @@ int main(){
     unsigned int N;
     scanf("%d", &N);
     while (N != 0){
-        cout << gcd(N) << endl;
+        printf("%d\n", gcd(N));
         scanf("%d", &N);
     }
     return 0;
--- _Testing files/Tuples.cpp
+++ _Testing files/Tuples.cpp
@@ -1,11 +1,15 @@
 #include<iostream>
 #include<tuple>
+#include<map>
 
 using namespace std;
 
 int main(){
-    tuple <int, int> tu;
-    tu = make_tuple(1,3);
-    cout <<  << endl;
+    tuple <int, int> tu(1,3);
+    map<tuple<int,int>,int> ma;
+    ma[tu] = 33;
+    if (ma.find(tu) != ma.end()){
+        cout << ma[tu] << endl;
+    }
     return 0;
 }
