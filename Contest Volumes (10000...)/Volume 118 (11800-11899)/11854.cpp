c046849a31f3c16f0b739ed12403272dd1d5fb61
Ersocaut <leonardo.soy.yo@hotmail.com>
2021-06-23 10:16:45 -0500

    11854 accepted

--- Contest Volumes (10000...)/Volume 118 (11800-11899)/11854.cpp
+++ Contest Volumes (10000...)/Volume 118 (11800-11899)/11854.cpp
@@ -5,20 +5,8 @@ using namespace std;
 int main(){
     int a, b, c;
     while (scanf("%d %d %d",&a, &b, &c)==3 && a != 0 && b != 0 && c != 0){
-        if (a%3 == 0){
-            if (b%4 == 0){
-                if (c%5 == 0){
-                    printf("right\n");
-                }else printf("wrong\n");
-            } else printf("wrong\n");
-        }
-        else if (a%4 == 0){
-            if (b%3 == 0){
-                if (c%5 == 0){
-                    printf("right\n");
-                } else printf("wrong\n");
-            } else printf("wrong\n");
-        }
+        if((a*a == b*b + c*c) || (b*b == c*c + a * a) || (c*c == a*a + b*b)) printf("right\n");
+        else printf("wrong\n");
     }
     return 0;
 }
