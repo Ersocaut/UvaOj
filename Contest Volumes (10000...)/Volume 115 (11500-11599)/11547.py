f41211b5343fd70ab482a45fe519079fe9fbcdf5
Ersocaut <leonardo.soy.yo@hotmail.com>
2021-06-26 21:12:05 -0500

    11547 accepted

---
+++ Contest Volumes (10000...)/Volume 115 (11500-11599)/11547.py
@@ -0,0 +1,6 @@
+from sys import stdin
+def main():
+    for i in range(int(stdin.readline().strip())):
+        m = int(stdin.readline().strip())
+        print(str(((((((((m*567)//9)+7492)*235)//47)-498))))[-2])
+main()
